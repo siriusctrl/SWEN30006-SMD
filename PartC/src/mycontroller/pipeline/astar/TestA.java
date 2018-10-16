87f44f83cfb99e2c3ded807b83ce14a81a796f5e
Kevin Xu <sahuang97@hotmail.comu>
2018-10-16 22:56:03 +1100

    change a*

--- PartC/src/mycontroller/pipeline/astar/TestA.java
+++ PartC/src/mycontroller/pipeline/astar/TestA.java
@@ -9,23 +9,25 @@ import utilities.Coordinate;
 
 public class TestA {
 	public static void testA() {
-		for (int i = 0; i < 6; i++) {
-			for (int j = 0; j < 6; j++) {
-				MapRecorder.cost[i][j] = 1;
-			}
-		}
-		MapRecorder.cost[1][0] = 999;
-		MapRecorder.cost[1][1] = 999;
+		for (int i = 0; i < 8; i++) {
+ 			for (int j = 0; j < 8; j++) {
+ 				MapRecorder.cost[i][j] = 1111;
+ 			}
+ 		}
+		MapRecorder.cost[1][0] = 9;
+ 		MapRecorder.cost[1][1] = 99;
 		MapRecorder.cost[1][2] = 999;
 		MapRecorder.cost[1][3] = 999;
 		MapRecorder.cost[1][4] = 999;
 		MapRecorder.cost[3][3] = 999;
 		MapRecorder.cost[3][4] = 999;
 		MapRecorder.cost[3][5] = 999;
-		Pathway p = AStar.findShortestPath(new Coordinate[] {new Coordinate(0, 0),new Coordinate(4, 1)});
+		MapRecorder.cost[2][5] = 99;
+ 		Pathway p = AStar.findShortestPath(new Coordinate[] {new Coordinate(0, 0),new Coordinate(5, 5)});
 		Stack<Coordinate> st = p.getPath();
 		while (!st.isEmpty()) {
 			System.out.println(st.pop());
 		}
+		System.out.println(p.getCost());
 	}
 }
